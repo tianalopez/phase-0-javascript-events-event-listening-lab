da463660183f6e4bf561a0c1706b49d15e9bb3e7
Tiana Lopez <tianadavin@gmail.com>
2023-09-13 10:47:40 -0700

    Add another solution

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+// function addingEventListener() {
+//   const input = document.getElementById('button');
+//   input.addEventListener('click', function() {
+//     alert('I was clicked');
+//   });
+// }
+// addingEventListener();
+
 function addingEventListener() {
   const input = document.getElementById('button');
-  input.addEventListener('click', function() {
-    alert('I was clicked');
-  });
+  input.addEventListener('click', () => alert('I was clicked'));
 }
-addingEventListener();
